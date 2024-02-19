68038ef5185fdf7f12f1e633c52618db43b897c2
amettier <157603984+amettier@users.noreply.github.com>
2024-02-19 15:52:23 +0100

    changes

--- P2P Lending/p2p-website/src/components/Signup/SignupElements.js
+++ P2P Lending/p2p-website/src/components/Signup/SignupElements.js
@@ -55,13 +55,13 @@ export const FormContent = styled.div`
 
 export const Form  = styled.form`
     background: #010101;
-    max-width: 400px;
+    max-width: 800px;
     height: auto;
     width: 100%;
     z-index: 1;
     display: grid;
     margin: 0 auto;
-    padding: 80px 32px;
+    padding: 25px 32px;
     border-radius: 4px;
     box-shadow: 0 1px 3px rgba(0,0,0,0.9);
 
@@ -80,12 +80,12 @@ export const FormH1 = styled.h1`
 
 export const FormLable = styled.label`
     margin-bottom: 8px;
-    font-size: 14px;
+    font-size: 10x;
     color: #fff;
 `;
 
 export const FormInput = styled.input`
-    padding: 16px 16px;
+    padding: 10px 16px;
     margin-bottom: 32px;
     border: none;
     border-radius: 4px;
--- P2P Lending/p2p-website/src/components/Signup/index.js
+++ P2P Lending/p2p-website/src/components/Signup/index.js
@@ -20,12 +20,24 @@ const SignUp = () => {
                 <Icon to="/">P2P-Lending</Icon> 
                 <FormContent>
                     <Form action="#">
-                        <FormH1>Registre</FormH1>
+                        <FormH1>Register</FormH1>
+                        <FormLable htmlFor='for'>Surname</FormLable>
+                        <FormInput type='text' required />
+                        <FormLable htmlFor='for'>Lastname</FormLable>
+                        <FormInput type='text' required />
                         <FormLable htmlFor='for'>Email</FormLable>
                         <FormInput type='email' required />
-                        <FormLable htmlFor = 'for'>Password</FormLable>
-                        <FormInput type='password' required />
-                        <FormButton type='submit'>Continue</FormButton>
+                        <FormLable htmlFor='for'>Address</FormLable>
+                        <FormInput type='text' required />
+                        <FormLable htmlFor='for'>City</FormLable>
+                        <FormInput type='text' required />
+                        <FormLable htmlFor='for'>PLZ</FormLable>
+                        <FormInput type='number' required />
+                        <FormLable htmlFor='for'>Date of birth</FormLable>
+                        <FormInput type='number' required />
+                        <FormLable htmlFor='for'>Walletaddress</FormLable>
+                        <FormInput type='text' required />
+                        <FormButton type='submit'>Sign Up</FormButton>
                         <Text>Forgot password</Text>
                     </Form>
                 </FormContent>
